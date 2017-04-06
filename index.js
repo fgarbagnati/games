76b44b3d63fadf0bb6b34df0e2321f179c9d5040
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-04-05 23:37:11 -0700

    Edits index.js back to original version

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 
-app.set('port', 3000);
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-var server = app.listen(app.get('port'), function() {
-	var port = server.address().port;
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
