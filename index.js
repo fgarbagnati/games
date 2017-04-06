b48b4e29b3a7e64c23497997a8fda86965f09cca
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-04-05 19:07:15 -0700

    Deletes old code and console.log

--- index.js
+++ index.js
@@ -8,10 +8,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(app.get('port'), function() {
 	var port = server.address().port;
-	console.log('Magic happens on port ' + port);
 });
-
-// app.use(express.static('public'));
-
-// var port = process.env.PORT || 3000;
-// app.listen(port);
